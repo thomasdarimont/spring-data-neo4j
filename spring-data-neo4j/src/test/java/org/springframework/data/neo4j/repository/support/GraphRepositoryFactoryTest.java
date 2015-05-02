13edfaa8e6dc8358dca8e00d99cbfa3c6d759f58
Vince Bickers <vince@mambofish.org>
2015-05-02 12:46:54 +0100

    polishing commit. note: we need to change master pom when API changes made to SDC

--- spring-data-neo4j/src/test/java/org/springframework/data/neo4j/repository/support/GraphRepositoryFactoryTest.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/repository/support/GraphRepositoryFactoryTest.java
@@ -11,6 +11,17 @@ import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.neo4j.repository.GraphRepositoryImpl;
 import org.springframework.transaction.annotation.Transactional;
 
+/*
+ * Copyright (c)  [2011-2015] "Pivotal Software, Inc." / "Neo Technology" / "Graph Aware Ltd."
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
