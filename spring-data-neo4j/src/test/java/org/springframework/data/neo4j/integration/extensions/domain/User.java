f0f97e488cce99c2361257bc2733b6b8d18c6ae5
Adam George <atg103@outlook.com>
2015-05-18 19:37:44 +0100

    DATAGRAPH-624 - Refactoring test code for conversion service into a different package

--- spring-data-neo4j/src/test/java/org/springframework/data/neo4j/integration/extensions/domain/User.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/integration/extensions/domain/User.java
@@ -1,20 +1,10 @@
 package org.springframework.data.neo4j.integration.extensions.domain;
 
-import org.neo4j.ogm.annotation.typeconversion.Convert;
-import org.neo4j.ogm.typeconversion.ByteArrayBase64Converter;
-
 /**
  * @author: Vince Bickers
  */
 public class User {
 
-    private Long id;
-
-    //@Convert(ByteArrayBase64Converter.class)
-    private byte[] profilePictureData;
-
-    public byte[] getProfilePictureData() {
-        return this.profilePictureData;
-    }
+    Long id;
 
 }
